4297eb9a96f08ce38afbc86872f669afd40eba3b
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2024-06-06 22:06:52 +0200

    trash commit

---
+++ src/components/auth/LoginForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+export function LoginForm({}: Props) {
+  return (
+    <div>
+        
+        LOgin form <br />
+        
+        login <hr />
+        password
+    </div>
+  )
+}
