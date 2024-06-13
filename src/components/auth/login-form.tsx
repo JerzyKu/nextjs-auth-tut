35871f5b1c11086bbc1e41f85f0df5b51715cedb
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2024-06-14 01:23:46 +0200

    save in tehe middle of no were

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,9 +1,31 @@
+"use client";
+
+import * as z from "zod";
 import React from "react";
-import { CardWrapper } from "./card-wrapper";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { LoginSchema } from "@/schemas";
+import { CardWrapper } from "@/components/auth/card-wrapper";
 
 type Props = {};
 
 export function LoginForm({}: Props) {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <CardWrapper
       headerLabel="Welcome back"
