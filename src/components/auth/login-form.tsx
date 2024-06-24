29b35b39fb5cee042ce52cf964dcbb9999695ca4
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2024-06-24 16:34:40 +0200

    finisch login form

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -4,6 +4,7 @@ import * as z from "zod";
 import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input" 
 import {
   Form,
   FormControl,
@@ -14,6 +15,7 @@ import {
 } from "../ui/form";
 import { LoginSchema } from "@/schemas";
 import { CardWrapper } from "@/components/auth/card-wrapper";
+import { Button } from "../ui/button";
 
 type Props = {};
 
@@ -33,9 +35,53 @@ export function LoginForm({}: Props) {
       backButtonHref="/auth/register"
       showSocial
     >
-      Login Form! <hr />
-      E-Mail <hr />
-      Password <hr />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+          <div className="space-y-4">
+            <FormField 
+              control={form.control}
+              name="email"
+              render={ ({field}) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                  <Input {...field} placeholder="john.doe@example.com" type="email"/>
+
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+              }
+            />
+
+            <FormField 
+              control={form.control}
+              name="password"
+              render={ ({field}) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                  <Input 
+                    {...field} 
+                    placeholder="******" 
+                    type="password"
+                    />
+
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+              }
+            />
+          </div>
+          <Button 
+            type="submit"
+            className="w-full"
+          >
+            Login
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 }
