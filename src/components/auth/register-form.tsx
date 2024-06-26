13d9bf7401de278e6fc7a027de180c24b39f4dc2
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2024-06-26 16:44:09 +0200

    frond end

--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -13,35 +13,37 @@ import {
   FormLabel,
   FormMessage,
 } from "../ui/form";
-import { LoginSchema } from "@/schemas";
+import { RegisterSchema } from "@/schemas";
 import { CardWrapper } from "@/components/auth/card-wrapper";
 import { Button } from "../ui/button";
 import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
-import { login } from "@/actions/login";
+import { register } from "@/actions/register";
+
 
 type Props = {};
 
-export function LoginForm({}: Props) {
+export function RegisterForm({}: Props) {
 
   const [error, setError] = useState<string | undefined>("")
   const [success, setSuccess] = useState<string | undefined>("")
   const [isPending, startTransition ] = useTransition()
 
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
     defaultValues: {
       email: "",
       password: "",
+      name: "",
     },
   });
 
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
     setError("")
     setSuccess("")
 
     startTransition(() => {
-      login(values)
+      register(values)
         .then(data => {
           setError(data.error)
           setSuccess(data.success)
@@ -52,14 +54,34 @@ export function LoginForm({}: Props) {
 
   return (
     <CardWrapper
-      headerLabel="Welcome back"
-      backButtonLabel="Don't have an account?"
-      backButtonHref="/auth/register"
+      headerLabel="Create an Account."
+      backButtonLabel="Already have an account?"
+      backButtonHref="/auth/login"
       showSocial
     >
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <div className="space-y-4">
+
+          <FormField 
+              control={form.control}
+              name="name"
+              render={ ({field}) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                  <Input 
+                    {...field} 
+                    disabled={isPending}
+                    placeholder="John Doe" 
+                    type="text"/>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+              }
+            />
+
             <FormField 
               control={form.control}
               name="email"
@@ -78,6 +100,8 @@ export function LoginForm({}: Props) {
               )
               }
             />
+
+            
 
             <FormField 
               control={form.control}
@@ -106,7 +130,7 @@ export function LoginForm({}: Props) {
             type="submit"
             className="w-full"
           >
-            Login
+            Create an account.
           </Button>
         </form>
       </Form>
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,8 +1,21 @@
 import * as z from "zod"
 
 export const LoginSchema = z.object({
-    email: z.string().email(),
+    email: z.string().email({
+        message: "Email is required."
+    }),
     password: z.string().min(1, {
         message: "Password is required."
+    })
+})
+export const RegisterSchema = z.object({
+    email: z.string().email({
+        message: "Email is required."
+    }),
+    password: z.string().min(6, {
+        message: "minimum 6 chrarcters required."
+    }),
+    name: z.string().min(1,{
+        message: "Name is required."
     })
 })
