b5c4a1509bd8d5f46e865482c92cb41792ccf7f0
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2024-08-02 01:02:03 +0200

    mothing to garbe about

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,11 +1,12 @@
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
-import { LoginSchema } from "./schemas";
+import { LoginSchema } from "@/schemas";
 
 export default {
     providers: [ Credentials({
         async authorize(credentials){
+            const vali = 'a'
         }
     })]
 }satisfies NextAuthConfig
